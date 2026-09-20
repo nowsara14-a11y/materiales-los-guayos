36a9ca889310d9357d3b0ae3b927571a606705a8
nowsara14-a11y <tu_correo@ejemplo.com>
2026-09-19 23:00:21 -0400

    error 400 solucionado

--- hardware/settings.py
+++ hardware/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-hids%%k#vrmq*1knb5bqeydc)6qc$ps$6hs6fdeiqx#9h=!b8-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['://materiales-los-guayos.onrender.com', '127.0.0.1', 'localhost', '*']
 
 
 # Application definition
