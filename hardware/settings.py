6a2875be1383d06363630a196ff555b16c1c7562
nowsara14-a11y <tu_correo@ejemplo.com>
2026-09-19 22:24:32 -0400

    Django y whitenoise para achivos estáticos

--- hardware/settings.py
+++ hardware/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,6 +117,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Al final del archivo, debajo de STATIC_ROOT
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
